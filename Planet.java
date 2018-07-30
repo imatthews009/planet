06eba00b35e7ddf44aa5d6778bf81149a4935d56
Ian Matthews <ian@flowinc.com>
2018-07-30 15:00:13 -0700

    adding project skeleton and Planet.java

---
+++ Planet.java
@@ -0,0 +1,15 @@
+/**
+ * Planet
+ */
+public class Planet {
+  double xxPos;
+  double yyPos;
+  double ssVel;
+  double yyVel;
+  double mass;
+  String imgFileName;
+  public static void main(String[] args) {
+    
+  }
+  
+}
