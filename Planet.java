cd0b488ae830147013383b87516d5bf4a33b677b
Ian Matthews <ian@flowinc.com>
2018-07-31 09:44:58 -0700

    added calDistance and calcForceExertedBy methods

--- Planet.java
+++ Planet.java
@@ -2,6 +2,7 @@
  * Planet
  */
 public class Planet {
+  // instance variables
   public double xxPos;
   public double yyPos;
   public double xxVel;
@@ -9,6 +10,9 @@ public class Planet {
   public double mass;
   public String imgFileName;
 
+  static final double gForce = 6.67e-11;
+
+  // constructor
   public Planet(double xP, double yP, double xV, double yV, double m, String img) {
     this.xxPos = xP;
     this.yyPos = yP;
@@ -18,6 +22,7 @@ public class Planet {
     this.imgFileName = img;
   }
 
+  // constructor used to create a copy of the planet
   public Planet(Planet p) {
     this.xxPos = p.xxPos;
     this.yyPos = p.yyPos;
@@ -25,6 +30,26 @@ public class Planet {
     this.yyVel = p.yyVel;
     this.mass = p.mass;
     this.imgFileName = p.imgFileName;
+  }
+
+  // helper method - distance between two planets
+  public double calcDistance(Planet p) {
+    double distance;
+
+    double dx = p.xxPos - this.xxPos;
+    double dy = p.yyPos - this.yyPos;
+    distance = Math.sqrt((dx*dx) + (dy*dy));
+
+    return distance;
+  }
+
+  public double calcForceExertedBy(Planet p) {
+    double force;
+    double distance = calcDistance(p);
+
+    force = (gForce*this.mass*p.mass)/(distance*distance);
+
+    return force;
   }
 
 
